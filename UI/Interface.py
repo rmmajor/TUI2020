de6238807f578fd7ee2ec697640112544e84eb5e
Roma Major <65121094+rmmajor@users.noreply.github.com>
2020-08-29 12:09:56 +0300

    чутка допилив інтерфейс

--- UI/Interface.py
+++ UI/Interface.py
@@ -1,14 +1,14 @@
 import sys
 from PyQt5 import QtWidgets, QtGui, QtCore
-from ditch import untitled
-from os.path import expanduser
+import os
 
 
 class MainWindow(QtWidgets.QTabWidget):
     def __init__(self):
         QtWidgets.QTabWidget.__init__(self)
 
-        self.resize(900, 500)
+        # self.resize(900, 500)
+        self.setFixedSize(900, 500)
 
 
 # клас, який робить з рандомного UI віджет, який можна засунути в в вкладку
@@ -27,6 +27,9 @@ class FirstTabContentVidget(TabContent):
         self.setInputsLabel = QtWidgets.QLabel(self)
         self.setFileButton = QtWidgets.QPushButton(self)
         self.setFileLabel = QtWidgets.QLabel(self)
+        self.showOutputPathLabel = QtWidgets.QLabel(self)
+        self.showOutputPathLine = QtWidgets.QLineEdit(self)
+        self.openOutputButton = QtWidgets.QPushButton(self)
 
         self.setupUi(self)
 
@@ -56,6 +59,19 @@ class FirstTabContentVidget(TabContent):
         # якшо кнопка нажата, визиваєся okButtonClicked
         self.okButton.clicked.connect(self.okButtonClicked)
 
+        self.showOutputPathLabel.setGeometry(QtCore.QRect(400, 60, 81, 20))
+        self.showOutputPathLabel.setObjectName("showOutputPathLabel")
+        self.showOutputPathLabel.setText("Відповідь тут:")
+
+        self.showOutputPathLine.setGeometry(QtCore.QRect(400, 90, 361, 20))
+        self.showOutputPathLine.setObjectName("showOutputPathLine")
+        # self.showOutputPathLine.setText("Тут міг бути путь до вашого ответа, но ви його не зробили")
+
+        self.openOutputButton.setGeometry(771, 89, 75, 22)
+        self.openOutputButton.setObjectName("openOutputButton")
+        self.openOutputButton.setText("Відкрити")
+        self.openOutputButton.clicked.connect(self.openOutputButtonClicked)
+
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def setFileButtonClicked(self):
@@ -69,18 +85,27 @@ class FirstTabContentVidget(TabContent):
         if dialog.exec_():
             filePath = dialog.selectedFiles()
             # print(filePath)
+        self.showOutputPath()
 
     def okButtonClicked(self):
         inputs = self.inputsTextField.toPlainText()
-        # print(inputs)
         self.saveDataToFile(inputs)
+        self.showOutputPath()
+
+    def openOutputButtonClicked(self):
+        os.startfile('input.txt')
 
     def saveDataToFile(self, data):
         with open("input.txt", 'w') as new_file:
             new_file.write(data)
+
+    def showOutputPath(self):
+        path = "C:\\Users\\Major\\Desktop\\Progs\\tui2020\\input.txt"
+        self.showOutputPathLine.setText(path)
 
 
 # фронт другого пункта завдання
 class SecondTabContentVidget(TabContent):
     def __init__(self):
         TabContent.__init__(self)
+        
